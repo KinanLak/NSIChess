9b34b0667633d2c1b0f66cd8fb081caf27b5242a
KinanLak <kinan.lakh@gmail.com>
2021-05-09 18:32:09 +0200

    SLD Fix

--- main.c
+++ main.c
@@ -8,9 +8,8 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 500
-
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
 
 void pause();
 
@@ -27,11 +26,11 @@ int main(int argc, char* argv[])
     //Initialisation of the video mode that we will use
     SDL_Init(SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow("Hello world", 50, 50, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_SWSURFACE);
+    window = SDL_CreateWindow("Hello world", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_SWSURFACE);
     
     windowSurface = SDL_GetWindowSurface(window);
 
-    bgImage = SDL_LoadBMP("bg.bmp");
+    bgImage = SDL_LoadBMP("test2.bmp");
 
     currentImage = bgImage;
 
