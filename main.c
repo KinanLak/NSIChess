72329f07b00ddbcfb613b6ff66ffe0acbfd7e38d
julienChemillier <66684524+julienChemillier@users.noreply.github.com>
2021-05-09 17:56:53 +0200

    test bg.bmp

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 //For minimisation of the window with a '-' -> SDL_WM_IconifyWindow()
 
 
+//For minimisation of the window with a '-' -> SDL_WM_IconifyWindow()
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
@@ -14,23 +17,29 @@ void pause();
 
 int main(int argc, char* argv[])
 {
-  //Initialisation of the video mode that we will use
-  SDL_Init(SDL_INIT_VIDEO);
-  
-  SDL_CreateWindow("Hello world", 50, 50, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_SWSURFACE);
+    //Variables initialisation
+    SDL_Window *window = NULL;
+    SDL_Surface *windowSurface = NULL;
+    SDL_Surface *bgImage = NULL;
+    SDL_Surface *currentImage = NULL;
 
-  pause();
 
-  //Quit SDL
-  SDL_Quit();
-  return EXIT_SUCCESS;
-}
+    //Initialisation of the video mode that we will use
+    SDL_Init(SDL_INIT_VIDEO);
 
-void pause()
-{
-    int continuer = 1;
+    window = SDL_CreateWindow("Hello world", 50, 50, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_SWSURFACE);
+    
+    windowSurface = SDL_GetWindowSurface(window);
+
+    bgImage = SDL_LoadBMP("bg.bmp");
+
+    currentImage = bgImage;
+
+
+
     SDL_Event event;
  
+    int continuer = 1;
     while (continuer)
     {
         SDL_WaitEvent(&event);
@@ -41,7 +50,23 @@ void pause()
             case SDL_KEYDOWN:
                 continuer = 0;
             case SDL_MOUSEBUTTONDOWN:
-                continuer = 0;
+                if (event.button.x <=100)
+                {
+                    continuer = 0;
+                }
         }
+        SDL_BlitSurface(currentImage, NULL, windowSurface, NULL);
+        SDL_UpdateWindowSurface(window);
     }
+
+    //We free the surface
+    SDL_FreeSurface(bgImage);
+    SDL_DestroyWindow(window);
+
+    bgImage = windowSurface = NULL;
+    window = NULL;
+
+    //Quit SDL
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
