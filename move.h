757247d39bd6b3fbcd8be74930616809cc595ceb
julienChemillier <66684524+julienChemillier@users.noreply.github.com>
2021-05-20 19:54:45 +0200

    Add files via upload

---
+++ move.h
@@ -0,0 +1,25 @@
+#ifndef H_FILE
+#define H_FILE
+
+//Creation of the structure for each move
+typedef struct MoveStructure MoveStructure;
+struct MoveStructure
+{
+    int departureCase;
+    int arrivalCase;
+    MoveStructure *nextMove;
+};
+
+//Creation of the structure wich store the first move
+typedef struct FileMoveStructure FileMoveStructure;
+struct FileMoveStructure
+{
+    MoveStructure *firstMove;
+};
+
+//Creation of the prototypes
+FileMoveStructure *initialise(void);
+void addMoveFile(FileMoveStructure *file, int newDepartureCase, int newArrivalCase);
+void storeAllMovesSQL(FileMoveStructure *file, int gameId);
+
+#endif
